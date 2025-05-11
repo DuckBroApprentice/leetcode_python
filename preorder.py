fe1dd1b8995e575fe2fce5d725dc9685ab76fe46
DuckBroApprentice <pigbirthin0320@gmail.com>
2025-05-11 16:17:57 +0800

    多叉樹preorder in stack

--- preorder.py
+++ preorder.py
@@ -22,3 +22,40 @@ class Solution(object):
         return res
 
 #runtime 47ms Memory 15.58MB
+
+#參考了一個想法很厲害
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if root == None:
+            return []
+        
+        #利用堆疊的思想
+        stack = [root]
+        #第一次處理時stack = [root{val,children[]}]
+        #pop: stack = []
+        #push: root.children with reverse : stack = [root.children[::-1]
+        #先進後出才會正確順序
+        res = []
+
+        while stack :
+            """
+            第一次寫的是：
+            res.append(stack.pop().val)
+            stack.extend(root.children[::-1])
+            這樣會造成記憶體溢出
+            原因在於永遠在跑第一個root
+            修改：
+            root = stack.pop()
+            res.append(root.val)
+            stack.extend(root.children[::-1])
+            用list處理也要記得前進
+            """
+            root = stack.pop()
+            res.append(root.val)
+            stack.extend(root.children[::-1])
+
+        return res
